b3c8e3fae315b75add92b53f01c29d17e0107ec1
Koala_Trinh <khoa.trinh@hotmail.com>
2022-04-01 15:55:24 +0700

    adding lambda funcion

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,13 +5,24 @@ import csv
 import os
 
 ## Parameters to run the script, their parameters either hard-code or put in environment variable ##
-session = boto3.Session(profile_name="default")
 document_name = "gethostnamelinux"
 landing_zone = "aws_lz212_ssm_history"
 env = "non-prod"
 year = date.today().year
 report_name = landing_zone + "-" + env + "-" + str(year) + ".csv"
+local_file_path = "/tmp/" + report_name
 bucket_name = "ses-mail1"
+
+## initial connection ##
+
+ssm = boto3.client("ssm")
+
+paginator_ssm = ssm.get_paginator("list_commands")
+
+marker = None
+pages = paginator_ssm.paginate(
+    PaginationConfig={"PageSize": 5, "StartingToken": marker}
+)
 
 
 class SSM_command:
@@ -22,34 +33,22 @@ class SSM_command:
         self.status = status
 
 
-ssm = session.client("ssm")
-
-paginator_ssm = ssm.get_paginator("list_commands")
-
-marker = None
-pages = paginator_ssm.paginate(
-    PaginationConfig={"PageSize": 5, "StartingToken": marker}
-)
-
-
 def download_from_s3(file_name):
-    s3 = session.resource("s3")
+    s3 = boto3.resource("s3")
     try:
         s3.Object(bucket_name, file_name).load()
     except botocore.exceptions.ClientError as e:
         if e.response["Error"]["Code"] == "404":
             return 1
     else:
-        s3 = session.client("s3")
-        s3.download_file(
-            bucket_name, file_name, "d:/1.REPO/Automation Tasks/" + file_name
-        )
+        s3 = boto3.client("s3")
+        s3.download_file(bucket_name, file_name, local_file_path)
         return 0
 
 
 def upload_file_to_bucket(bucket_name, file_path):
     try:
-        s3_resource = session.resource(service_name="s3")
+        s3_resource = boto3.resource(service_name="s3")
         file_dir, file_name = os.path.split(file_path)
 
         bucket = s3_resource.Bucket(bucket_name)
@@ -71,10 +70,20 @@ def write_result(list_commands):
         "status",
         "instances status",
     ]
+
+    total = len(list_commands)
+
     current_month = date.today().month
     report_existed = download_from_s3(report_name)
-    mode = "a" if os.path.exists(report_name) and current_month != 1 else "w"
-    with open(report_name, mode, encoding="UTF8", newline="") as csvfile:
+
+    mode = "a" if report_existed == 0 and current_month != 1 else "w"
+
+    before_append_lines = 0
+    if mode is "a":
+        with open(local_file_path) as f:
+            before_append_lines = sum(1 for line in f)
+
+    with open(local_file_path, mode, newline="") as csvfile:
         writer = csv.writer(csvfile)
         if current_month == 1 or report_existed == 1:
             writer.writerow(field_names)
@@ -90,12 +99,18 @@ def write_result(list_commands):
                 ]
             )
 
-    print(
-        upload_file_to_bucket(
-            bucket_name=bucket_name,
-            file_path="d:/1.REPO/Automation Tasks/" + report_name,
-        )
+    with open(local_file_path) as f:
+        after_append_lines = sum(1 for line in f)
+    return (
+        upload_file_to_bucket(bucket_name=bucket_name, file_path=local_file_path),
+        total,
+        report_existed,
+        mode,
+        before_append_lines,
+        after_append_lines,
     )
+
+    # return lines
 
 
 class SSM_command:
@@ -114,30 +129,29 @@ class SSM_command:
         self.ids_states = ids_states
 
 
-### MAIN ###
-cmd_history = []
+def lambda_handler(event, context):
 
-for page in pages:
-    list_ex_cmds = page["Commands"]
-    # filter the list_execute_commands with the document name only
-    filter_list_cmds = list(
-        filter(
-            lambda list_ex_cmds: list_ex_cmds["DocumentName"] == document_name,
-            list_ex_cmds,
-        )
-    )
-    if len(filter_list_cmds) <= 0:
-        continue
-    for exe_cmd in filter_list_cmds:
-        execution_command = SSM_command(
-            exe_cmd["CommandId"],
-            exe_cmd["DocumentName"],
-            exe_cmd["RequestedDateTime"],
-            exe_cmd["Status"],
-            exe_cmd["Targets"][0]["Values"],
-        )
-        cmd_history.append(execution_command)
+    cmd_history = []
 
-for cmd in cmd_history:
-    print(cmd.ids_states)
-# write_result(cmd_history)
+    for page in pages:
+        list_ex_cmds = page["Commands"]
+        # filter the list_execute_commands with the document name only
+        filter_list_cmds = list(
+            filter(
+                lambda list_ex_cmds: list_ex_cmds["DocumentName"] == document_name,
+                list_ex_cmds,
+            )
+        )
+        if len(filter_list_cmds) <= 0:
+            continue
+        for exe_cmd in filter_list_cmds:
+            execution_command = SSM_command(
+                exe_cmd["CommandId"],
+                exe_cmd["DocumentName"],
+                exe_cmd["RequestedDateTime"],
+                exe_cmd["Status"],
+                exe_cmd["Targets"][0]["Values"],
+            )
+            cmd_history.append(execution_command)
+
+    return len(cmd_history)  # write_result(cmd_history)
